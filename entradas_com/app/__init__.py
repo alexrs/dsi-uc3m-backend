b724965577d966e5cb90417b79ef248fa6b2bf84
Alejandro Rodriguez <alexrs95@gmail.com>
2016-04-08 11:21:29 +0200

    index

--- entradas_com/app/__init__.py
+++ entradas_com/app/__init__.py
@@ -21,11 +21,11 @@ def not_found(error):
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.mod_auth.controllers import mod_auth as auth_module
+from app.mod_main.controllers import mod_main as main_module
 
 # Register blueprint(s)
 app.register_blueprint(auth_module)
-# app.register_blueprint(xyz_module)
-# ..
+app.register_blueprint(main_module)
 
 # Build the database:
 # This will create the database file using SQLAlchemy
---
+++ entradas_com/app/mod_main/controllers.py
@@ -0,0 +1,10 @@
+# Import flask dependencies
+from flask import Blueprint, render_template
+
+# Define the blueprint: 'auth', set its url prefix: app.url/auth
+mod_main = Blueprint('index', __name__,)
+
+# Set the route and accepted methods
+@mod_main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("main/index.html")
