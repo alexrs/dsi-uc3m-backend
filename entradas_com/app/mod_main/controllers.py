290d21f9a6b942bf4a65df4a8f3fc7a25394d816
Alejandro Rodriguez <alexrs95@gmail.com>
2016-05-07 18:04:41 +0200

    Almost done

--- entradas_com/app/mod_main/controllers.py
+++ entradas_com/app/mod_main/controllers.py
@@ -12,19 +12,18 @@ def index():
 
 @mod_main.route('/login/', methods=['POST'])
 def login():
-	print "Login"
-	username = request.form['username']
+	email = request.form['email']
 	password = request.form['password']
-	user = User.query.filter_by(username=username).first()
-	if user.email == email:
+	user = User.query.filter_by(email=email).first()
+	if user is not None:
 		return render_template("main/index.html", user=user)
 	return render_template("main/index.html")
 
 @mod_main.route('/signup/', methods=['POST'])
 def signup():
-	print "Sign up"
-	username = request.form['username']
+	print "Sign"
 	email = request.form['email']
+	username = request.form['username']
 	password = request.form['password']
 	user = User(username, email, password)
 
