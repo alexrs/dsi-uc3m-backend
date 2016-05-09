c95405fbe10741d667c105a21ddda1bea110a6be
Alejandro Rodriguez <alexrs95@gmail.com>
2016-05-09 14:25:19 +0200

    Shop

--- entradas_com/app/__init__.py
+++ entradas_com/app/__init__.py
@@ -23,6 +23,7 @@ from app.mod_main.controllers import mod_main as main_module
 from app.mod_search.controllers import mod_search as search_module
 from app.mod_event.controllers import mod_event as event_module
 from app.mod_auth.controllers import mod_auth as auth_module
+from app.mod_purchase.controllers import mod_purchase as purchase_module
 
 
 # Register blueprint(s)
@@ -30,6 +31,8 @@ app.register_blueprint(main_module)
 app.register_blueprint(search_module)
 app.register_blueprint(event_module)
 app.register_blueprint(auth_module)
+app.register_blueprint(purchase_module)
+
 
 # Build the database:
 # This will create the database file using SQLAlchemy
---
+++ entradas_com/app/mod_purchase/controllers.py
@@ -0,0 +1,10 @@
+# Import flask dependencies
+from flask import Blueprint, render_template, redirect, request, make_response
+from app.models import *
+from app import db
+
+mod_purchase = Blueprint('purchase', __name__,)
+
+@mod_purchase.route('/purchase')
+def purchase():	
+	return render_template("shop/shop.html")
