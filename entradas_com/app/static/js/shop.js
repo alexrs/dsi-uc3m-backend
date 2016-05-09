c9ee6dedcfcb82627c72762e63e06cddaf924d1e
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-05-09 21:12:28 +0200

    purchase initiated 2

--- entradas_com/app/static/js/shop.js
+++ entradas_com/app/static/js/shop.js
@@ -1,10 +1,35 @@
 function selectSection(id) {
     $(".seat-section").removeClass( "selected-section" );
     switch(id) {
-        case 0: $("#section-bl").addClass( "selected-section" ); break;
-        case 1: $("#section-bm").addClass( "selected-section" ); break;
-        case 2: $("#section-br").addClass( "selected-section" ); break;
-        case 3: $("#section-mm").addClass( "selected-section" ); break;
-        case 4: $("#section-fm").addClass( "selected-section" ); break;
+        case 0: {
+            if($("#section-bl").hasClass("available-section")) {
+                $("#section-bl").addClass( "selected-section" );
+            }
+            break;
+        }
+        case 1: {
+            if($("#section-bm").hasClass("available-section")) {
+                $("#section-bm").addClass( "selected-section" );
+            }
+            break;
+        }
+        case 2: {
+            if($("#section-br").hasClass("available-section")) {
+                $("#section-br").addClass( "selected-section" );
+            }
+            break;
+        }
+        case 3: {
+            if($("#section-mm").hasClass("available-section")) {
+                $("#section-mm").addClass( "selected-section" );
+            }
+            break;
+        }
+        case 4: {
+            if($("#section-fm").hasClass("available-section")) {
+                $("#section-fm").addClass( "selected-section" );
+            }
+            break;
+        }
     }
 };
