efe59ef339403bb25645c61aac6346dd00dcb3a2
Chris Statkiewicz <r1ot.rog@gmail.com>
2016-05-10 12:30:52 +0200

    perdonad por el retraso

--- entradas_com/app/static/js/shop.js
+++ entradas_com/app/static/js/shop.js
@@ -35,6 +35,7 @@ function selectSection(id) {
     }
 };
 
+/*
 //regexes used to control forms in frontend
 var mail_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 var card_number = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
@@ -164,4 +165,4 @@ function enableSend(num){
        document.getElementById("shop-form-btn").disabled = true;
     }
     break;
-}
+}*/
